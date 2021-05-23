0e1b572d0a634e8df87439d6ef09547a831b1f2a
Nazar Matsevych <nazar6192111@gmail.com>
2021-05-23 23:00:47 +0300

    kickstarter landing page was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burgerToggler = document.querySelector('.burger-toggler');
+const nav = document.querySelector('.nav-burger');
+const button = document.querySelector('#burgerToggler');
+
+button.addEventListener('click', () => {
+  burgerToggler.classList.toggle('burger-toggler--active');
+  nav.classList.toggle('nav-burger--active');
+});
+
+const video = document.querySelector('video');
+
+video.onclick = () => video.paused ? video.play() : video.pause();
